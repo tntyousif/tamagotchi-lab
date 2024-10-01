43a56ecab25380697448aed5b099e0951f86f1ab
tntyousif <yousif02bh@gmail.com>
2024-10-01 16:44:52 +0300

    step 3

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+// 1) Define the required variables used to track the state of the game.
+
+// 2) Store cached element references.
+
+// 3) Upon loading, the game state should be initialized, and a function should 
+//    be called to render this game state.
+
+// 4) The state of the game should be rendered to the user.
+
+// 5) Handle the game over logic. 
+
+// 6) Handle each instance of a player clicking a button with the use of a 
+//    `handleClick()` function.
+
+// 7) Create reset functionality.
+
+
 /*-------------------------------- Constants --------------------------------*/
 const state = {
     boredom: "0",
@@ -5,6 +22,30 @@ const state = {
     sleepinss : "0"
 };
 
+//st2
+const boredomState = document.getElementById('boredom-stat');
+const hungerState = document.getElementById('hunger-stat');
+const sleepinessState = document.getElementById('sleepiness-stat');
+
+console.log(boredomState);
+console.log(hungerState);
+console.log(sleepinessState);
+
+const playButten = document.getElementById('play');
+const feedButten = document.getElementById('feed');
+const sleepButten = document.getElementById('sleep');
+
+console.log(playButten);
+console.log(feedButten);
+console.log(sleepButten);
+
+const gameMessageEl = document.getElementById('message');
+
+console.log(gameMessageEl);
+
+const resetBtnEl = document.getElementById('restart');
+
+console.log(resetBtnEl);
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -15,9 +56,23 @@ let gameOver;
 
 
 
+
 /*-------------------------------- Functions --------------------------------*/
 
+function init() {
+    console.log('init is called');
+    gameOver = false;
+    timer = setInterval(runGame, 2000);
 
+    render();
+}
 
+function runGame() {
+    console.log('The game is running!');
+}
+
+render();
 /*----------------------------- Event Listeners -----------------------------*/
+
+
 
